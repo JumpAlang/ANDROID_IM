b590b66d888414891c0cb64bcafa12322900e3c7
sunshinekao <sunshinekao@users.noreply.github.com>
2019-09-22 12:15:01 +0800

    自定义View
    旋转封面

--- app/src/main/java/com/example/mojitongxin/MainActivity.java
+++ app/src/main/java/com/example/mojitongxin/MainActivity.java
@@ -2,7 +2,12 @@ package com.example.mojitongxin;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import android.view.View;
+
+import com.example.mojitongxin.ui.RotateView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        RotateView rotateView = findViewById(R.id.RotateView);
+
+        Bitmap bitmap = BitmapFactory.decodeResource(getResources(),R.drawable.test1);
+
+        rotateView.setSource(bitmap);
     }
 }
